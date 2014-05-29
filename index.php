7a14e15058b06286061abe9fba0b15aad6f586d6
luismi <aquilafuska@yahoo.es>
2014-05-29 17:51:18 +0200

    index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Indice</title>
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="style.css" >
+
+</head>
+
+<body>
+	
+	<ul>
+		<li><a href="01-front-page.html">Front page</a></li>
+		<li><a href="02-page.html">Página</a></li>
+		<li><a href="03-full-width.html">Ancho total</a></li>
+		<li><a href="04-archives.html">Archivos</a></li>
+		<li><a href="05-contact.html">Contacto</a></li>
+		<li><a href="06-blog.html">Blog</a></li>
+		<li><a href="07-single.html">Single</a></li>
+		<li><a href="08-portafolio.html">Portafolio</a></li>
+		<li><a href="09-single-portafolio.html">Single portafolio</a></li>
+	</ul>
+
+	
+</body>
+</html>
